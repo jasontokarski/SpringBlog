6afdb4bd00e2ab50e13767ee5506cb54eb9a5052
Tokarski <jason.tokarski@capgemini.com>
2019-05-22 12:54:44 -0500

    Added External DB configuration

---
+++ src/main/java/com/capgemini/util/AppConfig.java
@@ -0,0 +1,28 @@
+package com.capgemini.util;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@PropertySource(value = {"classpath:db.properties"})
+public class AppConfig {
+	
+	@Autowired
+	private Environment env;
+	
+	@Bean
+	public DataSource getDataSource() {
+		final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(env.getProperty("db.driver"));
+		dataSource.setUrl(env.getProperty("db.jdbcUrl"));
+		dataSource.setUsername(env.getProperty("db.username"));
+		dataSource.setPassword(env.getProperty("db.password"));
+		return dataSource;
+	}
+}
