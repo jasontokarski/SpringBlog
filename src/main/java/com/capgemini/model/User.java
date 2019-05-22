a3ec7fa7855b04939749694c637999cd89d31cdd
Tokarski <jason.tokarski@capgemini.com>
2019-05-22 12:04:46 -0500

    Added JPA/Hibernate

--- src/main/java/com/capgemini/model/User.java
+++ src/main/java/com/capgemini/model/User.java
@@ -1,5 +1,39 @@
 package com.capgemini.model;
 
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+@Entity
+@Table(name = "user")
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private long id;
 	
+	@Column(name = "first_name")
+	@NotNull(message = "Please enter a first name.")
+	@Size(min = 2, max = 25)
+	private String firstName;
+	
+	@Column(name = "last_name")
+	@NotNull(message = "Please enter a last name.")
+	@Size(min = 2, max = 25)
+	private String lastName;
+	
+	@Column(name = "username", unique = true)
+	@NotNull(message = "Please enter a username.")
+	@Size(min = 3, max = 25)
+	private String username;
+	
+	@Column(name = "password")
+	@NotNull(message = "Please enter a password.")
+	@Size(min = 3, max = 25)
+	private String password;
+	
+	@Column(name = "email", unique = true)
+	@Email(message="Invalid email format.")
+	@NotNull(message = "Please enter an email address.")
+	@Size(min = 3, max = 25)
+	private String email;
 }
